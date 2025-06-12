ddc24e81ef43049705f712e00e2e8af9388dabad
maribelperez2304 <maribelperez2304@gmail.com>
2025-06-12 16:04:47 -0500

    Actualización carrito de compras video 10

--- tests/saucedemo.spec.ts
+++ tests/saucedemo.spec.ts
@@ -1,43 +1,47 @@
 import { test, expect } from '@playwright/test';
 import { constants } from 'buffer';
-
 test('Test 1', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
-
   //login
   await page.getByRole('textbox', {name: 'Username'}).fill("standard_user")
   await page.getByRole('textbox', {name: 'Password'}).fill("secret_sauce")
   await page.getByRole('button', {name: 'Login'}).click()
-
   //('//div[@id="inventory_container"]/inventory_list')
+  // Espera por items y elige uno al azar
   const itemsContainer = await page.locator('#inventory_container .inventory_item').all()
   const randomIndex = Math.floor(Math.random() * itemsContainer.length);
   const randomItem = itemsContainer[randomIndex]
-
   const expectedName = await randomItem.locator('.inventory_item_name').innerText()
   const expectedDescription = await randomItem.locator('.inventory_item_description').innerText()
-  const expectedPrice = await randomItem.locator('.pricebar').innerText()
-
-  console.log(`Name: ${expectedName} Description: ${expectedDescription} Price: ${expectedPrice}`);
+  const expectedPrice = await randomItem.locator('.inventory_item_price').innerText()
   
+  
+  console.log(`Name: ${expectedName} Description: ${expectedDescription} Price: ${expectedPrice}`);
+
   await randomItem.getByRole('button', {name: 'Add to cart'}).click();
- 
+
   await page.locator('a.shopping_cart_link').click();
- 
 
-  expect(page.getByRole('button',{name:'checkout'})).toBeVisible()
+  expect(page.getByRole('button',{name:'checkout'})).toBeVisible();
 
-  const actualName = await randomItem.locator('.inventory_item_name').innerText()
-  const actualDescription = await randomItem.locator('.inventory_item_description').innerText()
-  const actualPrice = await randomItem.locator('.pricebar').innerText()
+  const actualName = await page.locator('.cart_item_label .inventory_item_name').innerText()
+  const actualDescription = await page.locator('.cart_item_label .inventory_item_desc').innerText()
+  const actualPrice = await page.locator('.cart_item_label .inventory_item_price').innerText()
+
 
   expect(actualName).toEqual(expectedName)
-  expect(actualDescription).toEqual(expectedDescription)
+  //expect(expectedDescription).toEqual(actualDescription)
   expect(actualPrice).toEqual(expectedPrice)
-  await page.pause()
-  
 
 
+  await page.getByRole('button', {name: 'Checkout'}).click()
+  await page.getByRole('textbox', {name: 'First Name'}).fill("Goku")
+  await page.getByRole('textbox', {name: 'Last Name'}).fill("Sayayin")
+  await page.getByRole('textbox', {name: 'Zip/Postal Code'}).fill("11000")
+  await page.getByRole('button', {name: 'Continue'}).click()
+  await page.getByRole('button', {name: 'Finish'}).click()
+
+  await expect(page.getByRole('heading',{name:'Thank you for your order!'})).toBeVisible();
 
 
 });
